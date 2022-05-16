051906685370a2ce38d7f293335f7cd03e592530
Данила Семешкин <semyoshkin8v@mail.ru>
2022-05-16 22:20:56 +0300

    Добавьте файлы проекта.

---
+++ Cryptography_curse/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Cryptography_curse
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
