12c8ce3c3c5b2c9f2bfca05b692f0f0d1d227249
Данила Семешкин <semyoshkin8v@mail.ru>
2022-06-19 22:21:16 +0300

    IEncryptor

--- Cryptography_curse/Services/Interfaces/IEncryptor.cs
+++ Cryptography_curse/Services/Interfaces/IEncryptor.cs
@@ -2,6 +2,7 @@
 {
     public interface IEncryptor
     {
-
+        public void Encrypt(string sourcePath, string destinationPath, string password, int bufferSize = 102400);
+        public bool Decrypt(string sourcePath, string destinationPath, string password, int bufferSize = 102400);
     }
 }
